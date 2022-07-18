f9f149c1e516ad0f72445e6c40e0593d745bc922
Sol256-dev <sol.malinga84@gmail.com>
2022-07-18 18:18:45 +0300

    Version 1

---
+++ index.py
@@ -0,0 +1,35 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("lets play")
+score = 0
+
+answer = input("What does CPU Stand for? ")
+
+if answer.lower() == "central processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("Wrong!")
+
+answer = input("What does GPU Stand for? ")
+
+if answer.lower() == "graphics processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("Wrong!")
+
+answer = input("What does RAM Stand for? ")
+
+if answer.lower() == "random access memory":
+    print("correct!")
+    score += 1
+else:
+    print("Wrong!")
+
+print("your score is " + str(int((score/3)*100)) + "%")
